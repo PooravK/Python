8ac5cb5527afc8021ff0ffdf3cae69ffdefc3088
PooravK <boomster.work@gmail.com>
2025-10-05 18:54:55 +0530

    Added comments for future reference

--- ContactBook.py
+++ ContactBook.py
@@ -1,16 +1,18 @@
 import json
 
-try:
+# Adding comments for my own sanity
+
+try: # Load previous laoded values
     with open("ContactList.json", "r") as f:
         contacts = json.load(f)
-except FileNotFoundError:
+except FileNotFoundError: # For first run when file is not made yet
     contacts = {}
 
 while(True):
     user_inp = input("\n ---------- CONTACT LIST ---------- \nWhat action do you want to perform?\n1 - Add item\n2 - Remove item\n3 - View full list\n4 - Search a name\n5 - Exit\nEnter from 1-5: ")
     try:
         final_input = int(user_inp)
-    except ValueError:
+    except ValueError: # In case the user inputs anything other than an integer
         print("\nError: Enter an integer!")
         continue
     
@@ -18,13 +20,13 @@ while(True):
         name = input("\nEnter a name: ")
         numb = input("Enter their contact: ")
         
-        contacts[name.lower()] = numb
+        contacts[name.lower()] = numb # Lower so that no matter in what order the user types, lower or upper, the dictionary gives same value
         continue
     elif (final_input == 2):
         name = input("\nEnter a name: ")
 
         if name.lower() in contacts:
-            del contacts[name.lower()]
+            del contacts[name.lower()] # In case the contact is not in dictionary
         else:
             print("\nContact not found!")
         continue
@@ -37,7 +39,7 @@ while(True):
     elif (final_input == 4):
         name = input("\nEnter a name: ")
 
-        if name.lower() in contacts:
+        if name.lower() in contacts: # In case the contact is not in dictionary
             print("\nContact:", contacts[name])
         else:
             print("Contact not found!")
@@ -45,8 +47,8 @@ while(True):
     elif (final_input == 5):
         break
     else:
-        print("Select from 1-5 only!")
+        print("Select from 1-5 only!") # So the user only puts inputs between 1-5
         continue
 
-with open("ContactList.json", "w") as f:
+with open("ContactList.json", "w") as f: # To update the memory file with new list of dictionary
     json.dump(contacts, f)
